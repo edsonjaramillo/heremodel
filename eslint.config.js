ac7f68643c759906242d0db9668843fab8e51cbf
Edson Jaramillo <edsonajaramillo@gmail.com>
2026-01-28 21:13:53 -0600

    chore(eslint): disable two stylistic rules
    - What: Turn off the following ESLint rules in eslint.config.js
      - antfu/consistent-list-newline
      - style/jsx-closing-bracket-location
    
    - Why: These stylistic rules produced noisy/incorrect reports and conflicted with the project's existing formatting preferences (handled by our formatter/other rules). Disabling them reduces false positives and keeps linting focused on actionable issues.

--- eslint.config.js
+++ eslint.config.js
@@ -7,10 +7,12 @@ export default antfu({
 	react: true,
 	stylistic: { indent: 'tab', semi: true },
 	rules: {
+		'antfu/consistent-list-newline': ['off'],
 		'jsonc/sort-keys': ['off'],
 		'react-refresh/only-export-components': ['off'],
 		'style/arrow-parens': ['off'],
 		'style/comma-dangle': ['off'],
+		'style/jsx-closing-bracket-location': ['off'],
 		'style/quote-props': ['off'],
 		indent: ['off'],
 	},
